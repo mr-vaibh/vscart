34a063ef01fe0fe266cc5e864bbbf3b7b65e13c0
Mr. VaiBH <mrvaibh0@gmail.com>
2019-11-26 12:48:49 +0530

    final try to add name in table for today

--- shop/views.py
+++ shop/views.py
@@ -144,7 +144,7 @@ def handlerequest(request):
     if verify:
         if response_dict['RESPCODE'] == '01':
             print('order successful')
-            o = Orders.objects.filter(order_id=106)
+            o = Orders.objects.filter(name="Vaibhav Shukla")
         else:
             print('order was not successful because' + response_dict['RESPMSG'])
     return render(request, 'shop/paymentstatus.html', {'response': response_dict})
